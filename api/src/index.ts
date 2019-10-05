16b40a264d9e90e401bb619eecbc6959ae97f68c
Max Krieg <maxwell.krieg@gmail.com>
2019-10-05 09:59:03 -0600

    first commit

---
+++ api/src/index.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata'
+import { ApolloServer } from 'apollo-server-express'
+import * as Express from 'express'
+import { buildSchema, Resolver, Query } from 'type-graphql'
+import { createConnection } from 'typeorm'
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String, { nullable: true, description: 'Ohhh la la' })
+  async hello() {
+    return 'Hello world'
+  }
+}
+
+const main = async () => {
+  await createConnection()
+
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  })
+  const apolloServer = new ApolloServer({ schema })
+
+  const app = Express()
+
+  apolloServer.applyMiddleware({ app })
+
+  app.listen(4000, () => {
+    console.log('server start localhost:4000/graphql')
+  })
+}
+
+main()
