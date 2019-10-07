e71fc192079c8963397d204486cf1f7f8aedc337
Max Krieg <maxwell.krieg@gmail.com>
2019-10-07 08:37:48 -0600

    add login resolver and redis

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,20 +1,51 @@
 import 'reflect-metadata'
-import { ApolloServer } from 'apollo-server-express'
+import cors from 'cors'
 import Express from 'express'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
 import { createConnection } from 'typeorm'
 
+import redis from './redis'
 import { RegisterResolver } from './modules/user/Register'
+import { LoginResolver } from './modules/user/Login'
 
 const main = async () => {
   await createConnection()
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver],
+    resolvers: [RegisterResolver, LoginResolver],
   })
-  const apolloServer = new ApolloServer({ schema })
+  const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req }: any) => ({ req }),
+  })
 
   const app = Express()
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: 'https://localhost:3000',
+    }),
+  )
+
+  const RedisStore = connectRedis(session)
+  app.use(
+    session({
+      store: new RedisStore({ client: redis as any }),
+      name: 'qid',
+      secret: process.env.SESSION_SECRET || '',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+      },
+    }),
+  )
 
   apolloServer.applyMiddleware({ app })
 
