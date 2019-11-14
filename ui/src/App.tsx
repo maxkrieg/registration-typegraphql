eed4ea375ffa919fac5479bacc68bfe1a954c5cc
Max Krieg <maxwell.krieg@gmail.com>
2019-11-14 15:11:19 -0700

    Add react bootstrap and start page layout

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,26 +1,76 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 const App: React.FC = () => {
+  const sections = [
+    {
+      title: 'about',
+      description: 'lmgtfy',
+    },
+    {
+      title: 'portfolio',
+      description: 'lmgtfy',
+    },
+    {
+      title: 'blog',
+      description: 'lmgtfy',
+    },
+    {
+      title: 'skills',
+      description: 'lmgtfy',
+    },
+    {
+      title: 'work',
+      description: 'lmgtfy',
+    },
+    {
+      title: 'contact',
+      description: 'lmgtfy',
+    },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>test</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid style={{ padding: '0' }}>
+      <Row>
+        <Col>
+          <div style={{ height: '200px', backgroundColor: 'lightblue' }}>
+            Cool stuff with picture here
+          </div>
+        </Col>
+      </Row>
+      <Navbar expand="lg" sticky="top" bg="light" variant="light">
+        <Navbar.Brand>MAX KRIEG</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav>
+            {sections.map(section => (
+              <Nav.Link href={`#${section.title.toLowerCase()}`}>{section.title}</Nav.Link>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      {sections.map(section => (
+        <Row id={section.title}>
+          <Container fluid>
+            <Row>
+              <Col>
+                <h2>{section.title}</h2>
+                <p>{section.description}</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col>Column 1</Col>
+              <Col>Column 2</Col>
+            </Row>
+          </Container>
+        </Row>
+      ))}
+    </Container>
   )
 }
 
