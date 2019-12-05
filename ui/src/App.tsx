8c3f4fbdd8800d03fee0157f9fb8d4bc14f06a91
Max Krieg <maxwell.krieg@gmail.com>
2019-12-05 08:11:42 -0700

    ui: Add switch to router and add email confirmed landing page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
@@ -7,6 +7,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import User from './pages/User'
+import EmailConfirmed from './pages/EmailConfirmed'
 import { sections } from './content/home'
 
 const App: React.FC = () => {
@@ -25,10 +26,13 @@ const App: React.FC = () => {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Route path="/" exact component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-      <Route path="/user" component={User} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/user" exact component={User} />
+        <Route path="/user/confirm/:token" exact component={EmailConfirmed} />
+      </Switch>
     </Router>
   )
 }
---
+++ ui/src/pages/EmailConfirmed.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import { RouteComponentProps, useParams } from 'react-router-dom'
+import { gql } from 'apollo-boost'
+import { useMutation } from '@apollo/react-hooks'
+
+const confirmUserMutation = gql`
+  mutation confirmUser($token: String!) {
+    confirmUser(token: $token)
+  }
+`
+
+const EmailConfirmed: React.FC<RouteComponentProps> = props => {
+  const { token } = useParams()
+  const [isConfirmed, setConfirmed] = useState(false)
+
+  const [sendConfirmation] = useMutation(confirmUserMutation, {
+    onCompleted: _ => setConfirmed(true),
+  })
+
+  useEffect(() => {
+    sendConfirmation({
+      variables: { token },
+    })
+  }, [token, sendConfirmation])
+
+  return (
+    <div>
+      Token {token}
+      Is confirmed {isConfirmed}
+    </div>
+  )
+}
+
+export default EmailConfirmed
