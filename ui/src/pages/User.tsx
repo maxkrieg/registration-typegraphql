b57e24208928d133fa57b59fff7d7381147c06e9
Max Krieg <maxwell.krieg@gmail.com>
2019-12-10 07:41:53 -0700

    ui: add layout and styles to user page

--- ui/src/pages/User.tsx
+++ ui/src/pages/User.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './css/User.css'
 import { RouteComponentProps } from 'react-router-dom'
 import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
 import { User as UserEntity } from '../types/graphql.d'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Spinner from 'react-bootstrap/Spinner'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 const userQuery = gql`
   query getUser {
@@ -11,22 +17,43 @@ const userQuery = gql`
       firstName
       lastName
       fullName
+      email
     }
   }
 `
 
 const User: React.FC<RouteComponentProps> = props => {
-  const { loading, data, error } = useQuery<UserEntity>(userQuery)
+  const { loading, data, error } = useQuery<{ user: UserEntity }>(userQuery)
   if (error) {
-    console.dir(error.graphQLErrors)
+    return <div>Error loading user info</div>
   }
-  if (loading) {
-    console.log(loading)
+  let content
+  if (!data || loading) {
+    content = <Spinner animation="border" />
+  } else {
+    content = (
+      <>
+        <ListGroup>
+          <ListGroup.Item>
+            <strong>Name:</strong> {data.user.fullName}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <strong>Email:</strong> {data.user.email}
+          </ListGroup.Item>
+        </ListGroup>
+      </>
+    )
   }
-  if (data) {
-    console.log(data)
-  }
-  return <div>User home page</div>
+  return (
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col xs lg="6">
+          <h3 className="user_header">User Account</h3>
+          {content}
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default User
